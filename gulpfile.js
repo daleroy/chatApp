4fd26bbf20ca19b056821547b07c9a52dd471796
daleroy <david.a.leroy@gmail.com>
2016-10-06 11:22:58 -0700

    Adding static html to project
    - added task to gulpfile to copy index.html to build directory

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,19 @@ var browserify = require('browserify');
 var babelify = require('babelify');
 var source = require('vinyl-source-stream');
 
-gulp.task('default', function () {
+
+gulp.task('bundle', function() {
   return browserify('./src/app.js')
         .transform(babelify, {presets: ["es2015", "react"]})
         .bundle()
         .pipe(source('chatApp.js'))
         .pipe(gulp.dest('./build/'));
 });
+
+gulp.task('copyhtml', function() {
+   gulp.src('./src/index.html}')
+   .pipe(gulp.dest('./build/'));
+});
+
+gulp.task('default', ['bundle', 'copyhtml']);
+
